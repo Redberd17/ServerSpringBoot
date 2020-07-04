ae7f9018c3fc893173df3029677f1bdf56b9d565
Ekaterina <redberd_17.kata@mail.ru>
2020-07-05 00:30:26 +0300

    Minor add

--- src/com/chugunova/myproject/mapper/DreamsMapper.java
+++ src/com/chugunova/myproject/mapper/DreamsMapper.java
@@ -1,5 +1,5 @@
 package com.chugunova.myproject.mapper;
 
 public class DreamsMapper {
-    public static String GET_ALL_USER_DREAMS = "select dream_name as dreamName, dream_date as dreamDate, dream_text as dreamText, dream_duration as dreamDuration\nfrom dreams\nwhere id_user in (select id_user from users where user_name = ?)";
+    public static String GET_ALL_USER_DREAMS = "select dream_name as dreamName, dream_date as dreamDate, dream_text as dreamText, dream_duration as dreamDuration\nfrom dreams\nwhere id_user in (select id_user from users where user_name = ?) order by dreamDate DESC";
 }
